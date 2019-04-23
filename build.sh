5987420567d43a2bdae1420ed7dd5cd816f809e2
Jonas Tranberg <jonas_ts@msn.com>
2019-04-24 00:34:43 +0200

    some updates...

--- build.sh
+++ build.sh
@@ -4,11 +4,8 @@ SRC_IMG=$1
 TARGET_IMG=$2
 TMP_IMG=$TARGET_IMG.tmp
 
-SECTOR_SIZE=512
 LOOP_DEV="/dev/loop2"
-TMP_IMG_SIZE="4G"
-IMG_SPARE_SPACE="200M"
-MNT_POINT=/mnt/pi
+MNT_POINT="/mnt/pi"
 
 # Check if inputs are given
 if [ -z "$SRC_IMG" ]; then
@@ -26,101 +23,64 @@ if [ -z "$TARGET_IMG" ]; then
     exit -1
 fi
 
+for CMD in qemu-arm-static fdisk parted resize2fs e2fsck
+do
+    if [ -z "$(command -v $CMD)" ]; then
+        echo "Missing $CMD, please install"
+        exit -1
+    fi
+done
+
 # Check if qemu-arm-static is available
 if [ -z "$(command -v qemu-arm-static)" ]; then
-    echo "qemu-arm-static binary not found!"
+    echo "qemu-arm-static"
     exit -1
 fi
 
-# Error handling
-
-function clean {
-    sudo losetup -d $LOOP_DEV > /dev/null 2>&1
-    
-    sudo rm $MNT_POINT/usr/bin/qemu-arm-static > /dev/null 2>&1
-    sudo umount $MNT_POINT > /dev/null 2>&1
+function cleanup {
     sync
+    sudo umount $MNT_POINT/boot > /dev/null 2>&1
+    sudo umount $MNT_POINT > /dev/null 2>&1
+    sudo losetup -d $LOOP_DEV > /dev/null 2>&1
     sudo rm $MNT_POINT -r > /dev/null 2>&1
+}
 
+function error {
+    echo "An error accoured, exiting..."
+    cleanup
     exit -1
 }
 
-trap "clean" ERR
-
-# Find root sector start
-SECTOR_START=$(($(fdisk -l $SRC_IMG | grep "^$SRC_IMG" | tail -n +2 | awk -F" "  '{ print $2 }') * $SECTOR_SIZE))
+# Error handling
+trap "error" ERR
 
 # Make tmp image
 cp $SRC_IMG $TMP_IMG
 
-# Increase tmp image size
-ORIG_SIZE=$(du -h $TMP_IMG | awk -F" " '{print $1}')
-DIFF_SIZE=$[$(numfmt --from=iec $TMP_IMG_SIZE) - $(numfmt --from=iec $ORIG_SIZE)]
+# Increase tmp image size with 1G
+sudo dd if=/dev/zero bs=1G count=2 >> $TMP_IMG
+sudo parted $TMP_IMG resizepart 2 100%
 
-dd if=/dev/zero bs=1 count=1 seek=$DIFF_SIZE of=$TMP_IMG
-parted $TMP_IMG resizepart 2 100%
-
-sudo losetup --offset=$SECTOR_START $LOOP_DEV $TMP_IMG
-sudo e2fsck -f $LOOP_DEV
-sudo resize2fs -f $LOOP_DEV
-sudo losetup -d $LOOP_DEV
+sudo losetup --partscan $LOOP_DEV $TMP_IMG
+sudo e2fsck -fy $LOOP_DEV"p2"
+sudo resize2fs -f $LOOP_DEV"p2"
 
 # Mount the image
 sudo mkdir -p $MNT_POINT
-sudo mount -o loop,offset=$SECTOR_START $TMP_IMG $MNT_POINT
+sudo mount $LOOP_DEV"p2" $MNT_POINT
+sudo mount $LOOP_DEV"p1" $MNT_POINT/boot
 
 # Copy root dir into image
-cp ./root/* $MNT_POINT -r
-
-# Copy qemu-arm-static binary
-sudo cp $(which qemu-arm-static) $MNT_POINT/usr/bin
+cp ./fs/* $MNT_POINT -r
 
 # chroot into raspbian and run commands
-sudo systemd-nspawn -D $MNT_POINT /bin/bash << EOF
-# Add node 11 repo
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-
-# Install dependencies
-apt update
-apt install -y vim matchbox-window-manager unclutter nitrogen chromium-browser xserver-xorg xinit rpd-plym-splash xdotool nodejs
-apt clean
-
-# Set plymouth splash screen
-rm /usr/share/plymouth/themes/pix/splash.png
-ln -s /home/pi/background.png /usr/share/plymouth/themes/pix/splash.png
-
-# Set auto login for tty1-3
-sed /etc/systemd/system/autologin@.service -i -e "s#^ExecStart=-/sbin/agetty --autologin [^[:space:]]*#ExecStart=-/sbin/agetty --autologin pi#"
-ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
-ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty2.service
-ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty3.service
-
-systemctl set-default multi-user.target
-
-# Enable ssh
-systemctl enable ssh
-
-# Quiet motd
-rm /etc/profile.d/sshpwd.sh
-echo | sudo tee /etc/motd
-
-# Set hostname
-hostnamectl set-hostname pup
-sed -i 's/raspberrypi/pup/g' /etc/hosts
-
-# Goodbye
-exit
-EOF
-
-# Clean up
-sudo rm $MNT_POINT/usr/bin/qemu-arm-static
-sudo umount $MNT_POINT
-sync
-sudo rm $MNT_POINT -r
+sudo systemd-nspawn --bind /usr/bin/qemu-arm-static -qD $MNT_POINT /home/pi/setup.sh
 
 # Shrink image
-SECTOR_END=$(($(($(fdisk -l $TMP_IMG | grep "^$TMP_IMG" | tail -n +2 | awk -F" "  '{ print $3 }') + 1)) * $SECTOR_SIZE))
-truncate --size=$SECTOR_END $TMP_IMG
+# resize2fs -M $LOOP_DEV
+
+# Clean up mounts
+cleanup
 
 # Move tmp image
 mv $TMP_IMG $TARGET_IMG
---
+++ fs/home/pi/setup.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# Safeguard
+if [ "$(command -v raspi-config)" ]; then
+    echo "Script not running on raspbian, exiting..."
+    exit -1
+fi
+
+# Add node 11 repo
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+
+# Install dependencies
+apt update
+apt install -y vim matchbox-window-manager unclutter nitrogen chromium-browser xserver-xorg xinit rpd-plym-splash xdotool nodejs
+apt clean
+
+# Set plymouth splash screen
+rm /usr/share/plymouth/themes/pix/splash.png
+ln -s /home/pi/background.png /usr/share/plymouth/themes/pix/splash.png
+
+# Set auto login for tty1-3
+sed /etc/systemd/system/autologin@.service -i -e "s#^ExecStart=-/sbin/agetty --autologin [^[:space:]]*#ExecStart=-/sbin/agetty --autologin pi#"
+ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty2.service
+ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty3.service
+
+systemctl set-default multi-user.target
+
+# Enable ssh
+systemctl enable ssh
+
+# Quiet motd
+rm /etc/profile.d/sshpwd.sh
+echo | sudo tee /etc/motd
+
+# Quiet boot
+echo "dwc_otg.lpm_enable=0 console=serial0,115200 console=tty3 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0" > /boot/cmdline.txt
+echo -e "\ndisable_splash=1" >> /boot/config.txt
+
+# Fix fstab
+cat >/etc/fstab <<EOL
+proc            /proc           proc    defaults          0       0
+/dev/mmcblk0p1  /boot           vfat    defaults          0       2
+/dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
+EOL
+
+# Goodbye (delete setup.sh)
+rm -- "$0"
+exit
